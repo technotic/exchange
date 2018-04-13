69c20b9751f19643817a363c45abbcc93dd0c62a
technotic <paul.d.taylor@gmail.com>
2018-04-13 08:24:20 +0100

    Simplest code that compiles and passes tests.

--- src/test/java/technotic/exchange/acceptance/AcceptanceTest.java
+++ src/test/java/technotic/exchange/acceptance/AcceptanceTest.java
@@ -24,21 +24,23 @@ public class AcceptanceTest {
         placeOrder(SELL, 1000, "VOD.L", "100.2", "User1", 1);
 
         assertThat(orderStore.openInterest("VOD.L", SELL), equalTo(asList(openInterest(1000, "100.2"))));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("0")));
+        verifyAverageExecutionPrice("VOD.L", "0");
+        verifyExecutedQuantity("VOD.L", "User1", 0);
+        verifyExecutedQuantity("VOD.L", "User2", 0);
 
         placeOrder(BUY, 1000, "VOD.L", "100.2", "User2", 2);
 
         assertThat(orderStore.openInterest("VOD.L", SELL), equalTo(emptyList()));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("100.2000")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(-1000));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(1000));
+        verifyAverageExecutionPrice("VOD.L", "100.2000");
+        verifyExecutedQuantity("VOD.L", "User1", -1000);
+        verifyExecutedQuantity("VOD.L", "User2", 1000);
 
         placeOrder(BUY, 1000, "VOD.L", "99", "User1", 3);
 
         assertThat(orderStore.openInterest("VOD.L", BUY), equalTo(asList(openInterest(1000, "99"))));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("100.2000")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(-1000));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(1000));
+        verifyAverageExecutionPrice("VOD.L", "100.2000");
+        verifyExecutedQuantity("VOD.L", "User1", -1000);
+        verifyExecutedQuantity("VOD.L", "User2", 1000);
 
         placeOrder(BUY, 1000, "VOD.L", "101", "User1", 4);
 
@@ -47,9 +49,9 @@ public class AcceptanceTest {
                         openInterest(1000, "99"),
                         openInterest(1000, "101"))
         ));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("100.2000")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(-1000));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(1000));
+        verifyAverageExecutionPrice("VOD.L", "100.2000");
+        verifyExecutedQuantity("VOD.L", "User1", -1000);
+        verifyExecutedQuantity("VOD.L", "User2", 1000);
 
         placeOrder(SELL, 500, "VOD.L", "102", "User2", 5);
 
@@ -62,9 +64,9 @@ public class AcceptanceTest {
                 asList(
                         openInterest(500, "102"))
         ));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("100.2000")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(-1000));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(1000));
+        verifyAverageExecutionPrice("VOD.L", "100.2000");
+        verifyExecutedQuantity("VOD.L", "User1", -1000);
+        verifyExecutedQuantity("VOD.L", "User2", 1000);
 
         placeOrder(BUY, 500, "VOD.L", "103", "User1", 6);
 
@@ -74,9 +76,9 @@ public class AcceptanceTest {
                         openInterest(1000, "101"))
         ));
         assertThat(orderStore.openInterest("VOD.L", SELL), equalTo(emptyList()));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("101.1333")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(-500));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(500));
+        verifyAverageExecutionPrice("VOD.L", "101.1333");
+        verifyExecutedQuantity("VOD.L", "User1", -500);
+        verifyExecutedQuantity("VOD.L", "User2", 500);
 
         placeOrder(SELL, 1000, "VOD.L", "98", "User2", 7);
 
@@ -85,9 +87,17 @@ public class AcceptanceTest {
                         openInterest(1000, "99"))
         ));
         assertThat(orderStore.openInterest("VOD.L", SELL), equalTo(emptyList()));
-        assertThat(orderStore.averageExecutionPrice("VOD.L"), equalTo(bd("99.8800")));
-        assertThat(orderStore.executedQuantity("VOD.L", "User1"), equalTo(500));
-        assertThat(orderStore.executedQuantity("VOD.L", "User2"), equalTo(-500));
+        verifyAverageExecutionPrice("VOD.L", "99.8800");
+        verifyExecutedQuantity("VOD.L", "User1", 500);
+        verifyExecutedQuantity("VOD.L", "User2", -500);
+    }
+
+    private void verifyAverageExecutionPrice(String ric, String expectedValue) {
+        assertThat(orderStore.averageExecutionPrice(ric), equalTo(bd(expectedValue)));
+    }
+
+    private void verifyExecutedQuantity(String ric, String user, int expectedValue) {
+        assertThat(orderStore.executedQuantity(ric, user), equalTo(expectedValue));
     }
 
     private void placeOrder(Direction direction, int quantity, String ric, String price, String user, long timestamp) {
